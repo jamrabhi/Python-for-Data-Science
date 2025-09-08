063d15ccf291a729c491365fd8f592ae52bcc9ed
Jamal RABHIOUAN <jamrabhi@f4r7s2.clusters.42paris.fr>
2025-09-08 17:12:38 +0200

    add 0/ex01

---
+++ Python-0-Starting/ex01/format_ft_time.py
@@ -0,0 +1,8 @@
+import time
+import datetime
+
+currtime = time.time()
+print(f"Seconds since January 1, 1970: {currtime:,.4f} or {currtime:.2e} in scientific notation")
+
+now = datetime.date.today()
+print(now.strftime("%b %d %Y"))
