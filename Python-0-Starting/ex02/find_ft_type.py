740c2ad1e67949dd131cf455b34659ca11d3ee62
Jamal RABHIOUAN <jamrabhi@f4r11s4.clusters.42paris.fr>
2025-09-11 18:59:58 +0200

    add 0/ex02

--- Python-0-Starting/ex02/find_ft_type.py
+++ Python-0-Starting/ex02/find_ft_type.py
@@ -1,8 +1,8 @@
 def all_thing_is_obj(object: any) -> int:
 	if isinstance(object, (list, tuple, set, dict)):
-		print(f"{object} {type(object)}")
+		print(f"{type(object).__name__.capitalize()} : {type(object)}")
 	elif isinstance(object, str):
 		print(f"{object} is in the kitchen : {type(object)}")
 	else:
 		print("Type not found")
-		return 42
+	return 42
