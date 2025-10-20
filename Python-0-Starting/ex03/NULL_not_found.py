8c023d87a2cc8e1b397f31835e68ac3f6c8972e3
Jamal RABHIOUAN <jamrabhi@f4r10s15.clusters.42paris.fr>
2025-10-20 18:27:12 +0200

    add 0/ex03

---
+++ Python-0-Starting/ex03/NULL_not_found.py
@@ -0,0 +1,15 @@
+def	NULL_not_found(object: any) -> int:
+	if object is None:
+		print(f"Nothing: {object} {type(object)}")
+	elif (isinstance(object, float) and object != object):
+		print(f"Cheese: {object} {type(object)}")
+	elif (isinstance(object, (int)) and object == 0):
+		print(f"Zero: {object} {type(object)}")
+	elif (isinstance(object, str) and object == ''):
+		print(f"Empty: {type(object)}")
+	elif (isinstance(object, bool) and object is False):
+		print(f"Fake: {object} {type(object)}")
+	else:
+		print("Type not found")
+		return 1
+	return 0
