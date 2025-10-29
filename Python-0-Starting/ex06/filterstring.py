19509cb75cf8f0301aaed7f36f90da497371317f
Jamal RABHIOUAN <jamrabhi@f4r10s4.clusters.42paris.fr>
2025-10-29 17:32:32 +0100

    add 0/ex06

--- Python-0-Starting/ex06/filterstring.py
+++ Python-0-Starting/ex06/filterstring.py
@@ -1,19 +1,18 @@
-"""Program accepts two arguments: a string(S), and an integer(N).
-The program should output a list of words from S that have a length greater
-than N.
-• Words are separated from each other by space characters.
-• Strings do not contain any special characters. (Punctuation or invisible)
-• The program must contain at least one list comprehension expression and one
-lambda.
-• If the number of argument is different from 2, or if the type of any argument
-is wrong, the program prints an AssertionError."""
-
-
 import sys
 from ft_filter import ft_filter
 
 
 def main():
+    """Program accepts two arguments: a string(S), and an integer(N).
+    The program should output a list of words from S that have a length greater
+    than N.
+    • Words are separated from each other by space characters.
+    • Strings do not contain any special characters. (Punctuation or invisible)
+    • The program must contain at least one list comprehension expression and
+      one lambda.
+    • If the number of argument is different from 2, or if the type of any
+      argument is wrong, the program prints an AssertionError."""
+
     try:
         assert len(sys.argv) == 3, "the arguments are bad"
 
--- Python-0-Starting/ex06/ft_filter.py
+++ Python-0-Starting/ex06/ft_filter.py
@@ -1,10 +1,9 @@
-"""filter(function or None, iterable) --> filter object
-
-Return an iterator yielding those items of iterable for which function(item)
-is true. If function is None, return the items that are true."""
-
-
 def ft_filter(func, iterable):
+    """filter(function or None, iterable) --> filter object
+
+    Return an iterator yielding those items of iterable for which function
+    (item) is true. If function is None, return the items that are true."""
+
     if func is None:
         temp_list = [item for item in iterable if item]
     else:
