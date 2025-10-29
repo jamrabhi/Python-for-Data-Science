dd9e28ec419d093ca8286076744e37c3c461d5cd
Jamal RABHIOUAN <jamrabhi@f4r10s4.clusters.42paris.fr>
2025-10-29 18:38:19 +0100

    add 0/ex07

---
+++ Python-0-Starting/ex07/sos.py
@@ -0,0 +1,38 @@
+import sys
+
+
+def main():
+    """Make a program that takes a string as an argument and encodes it into
+    Morse Code.
+    • The program supports space and alphanumeric characters
+    • An alphanumeric character is represented by dots . and dashes -
+    • Complete morse characters are separated by a single space
+    • A space character is represented by a slash /
+    You must use a dictionary to store your morse code.
+    If the number of arguments is different from 1, or if the type of any
+    argument is wrong, the program prints an AssertionError."""
+
+    NESTED_MORSE = {
+        'A': '.-',    'B': '-...',  'C': '-.-.',  'D': '-..',
+        'E': '.',     'F': '..-.',  'G': '--.',   'H': '....',
+        'I': '..',    'J': '.---',  'K': '-.-',   'L': '.-..',
+        'M': '--',    'N': '-.',    'O': '---', 'P': '.--.',
+        'Q': '--.-',  'R': '.-.',   'S': '...',   'T': '-',
+        'U': '..-',   'V': '...-',  'W': '.--',   'X': '-..-',
+        'Y': '-.--',  'Z': '--..',
+        '0': '-----', '1': '.----', '2': '..---', '3': '...--',
+        '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+        '8': '---..', '9': '----.', ' ': '/'
+    }
+
+    try:
+        assert len(sys.argv) == 2, "the arguments are bad"
+
+        assert all(c.isalnum() or c == ' ' for c in sys.argv[1]), "the arguments are bad"
+
+    except AssertionError as err:
+        print(f"AssertionError: {err}")
+
+
+if __name__ == "__main__":
+    main()
