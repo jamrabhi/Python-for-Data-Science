bc1f9967cf50102ea687781b7a2e8efc06b59c09
Jamal RABHIOUAN <jamrabhi@f4r10s1.clusters.42paris.fr>
2025-11-10 16:40:33 +0100

    add 0/ex08

--- Python-0-Starting/ex08/Loading.py
+++ Python-0-Starting/ex08/Loading.py
@@ -1,2 +1,9 @@
 def ft_tqdm(lst: range) -> None:
-    
+    '''The function must copy the function tqdm with the yield operator.'''
+    total = len(lst)
+    for i, item in enumerate(lst, 0):
+        percent = i / total * 100
+        bar = "█" * int(i / total * 100) + "-" * (100 - int(i / total * 100))
+        print(f"\r{percent:3.0f}%|{bar}|", end="")
+        yield item
+    print()
