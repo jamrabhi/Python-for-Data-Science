f895d6c23a0b6a3279198991591e26fed8f050ce
Jamal RABHIOUAN <jamrabhi@f4r10s8.clusters.42paris.fr>
2025-11-19 17:52:53 +0100

    add 0/ex08

--- Python-0-Starting/ex08/Loading.py
+++ Python-0-Starting/ex08/Loading.py
@@ -1,9 +1,11 @@
 def ft_tqdm(lst: range) -> None:
     '''The function must copy the function tqdm with the yield operator.'''
     total = len(lst)
-    for i, item in enumerate(lst, 0):
-        percent = i / total * 100
-        bar = "█" * int(i / total * 100) + "-" * (100 - int(i / total * 100))
-        print(f"\r{percent:3.0f}%|{bar}|", end="")
-        yield item
+    if total == 0:
+        return
+    for i, element in enumerate(lst, 1):
+        progress = i / total
+        bar = "█" * int(progress * 100) + " " * (100 - int(progress * 100))
+        print(f"{progress * 100:3.0f}%|{bar}| {i}/{total}", end="\r")
+        yield element
     print()
--- Python-0-Starting/ex08/tester.py
+++ Python-0-Starting/ex08/tester.py
@@ -3,8 +3,8 @@ from tqdm import tqdm
 from Loading import ft_tqdm
 
 for elem in ft_tqdm(range(333)):
-    sleep(0.09)
+    sleep(0.005)
 print()
 for elem in tqdm(range(333)):
-    sleep(2)
+    sleep(0.005)
 print()
