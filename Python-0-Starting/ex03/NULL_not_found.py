201d69c09c980d2f88b3ad510d77015529f0374a
Jamal RABHIOUAN <jamrabhi@f4r10s9.clusters.42paris.fr>
2025-12-04 15:40:16 +0100

    fix 0/ex03

--- Python-0-Starting/ex03/NULL_not_found.py
+++ Python-0-Starting/ex03/NULL_not_found.py
@@ -3,12 +3,12 @@ def	NULL_not_found(object: any) -> int:
 		print(f"Nothing: {object} {type(object)}")
 	elif (isinstance(object, float) and object != object):
 		print(f"Cheese: {object} {type(object)}")
+	elif (isinstance(object, bool) and object is False):
+		print(f"Fake: {object} {type(object)}")
 	elif (isinstance(object, (int)) and object == 0):
 		print(f"Zero: {object} {type(object)}")
 	elif (isinstance(object, str) and object == ''):
-		print(f"Empty: {type(object)}")
-	elif (isinstance(object, bool) and object is False):
-		print(f"Fake: {object} {type(object)}")
+		print(f"Empty: {object} {type(object)}")
 	else:
 		print("Type not found")
 		return 1
