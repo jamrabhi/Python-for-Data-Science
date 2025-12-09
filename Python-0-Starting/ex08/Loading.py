90318dd5f619700a8f57fb2e3b6bb83ed46d6920
Jamal RABHIOUAN <jamrabhi@f6r4s8.clusters.42paris.fr>
2025-12-09 18:10:30 +0100

    fix new subject

--- Python-0-Starting/ex08/Loading.py
+++ Python-0-Starting/ex08/Loading.py
@@ -1,11 +1,18 @@
+import os
+
 def ft_tqdm(lst: range) -> None:
     '''The function must copy the function tqdm with the yield operator.'''
     total = len(lst)
     if total == 0:
         return
+    try:
+        len_term = max(60, os.get_terminal_size().columns - 40)
+    except OSError:
+        len_term = 60
     for i, element in enumerate(lst, 1):
         progress = i / total
-        bar = "█" * int(progress * 60) + " " * (60 - int(progress * 60))
+        bar = "█" * int(progress * len_term) + " " * (len_term -
+                                                      int(progress * len_term))
         print(f"\r{progress * 100:3.0f}%|{bar}| {i}/{total}", end="")
         yield element
     print()
