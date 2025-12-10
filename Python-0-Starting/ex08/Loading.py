970428bf9e5fdc806a26967d665294dc6b85ada0
Jamal RABHIOUAN <jamrabhi@f1br6s6.clusters.42paris.fr>
2025-12-10 15:33:59 +0100

    fix new subject

--- Python-0-Starting/ex08/Loading.py
+++ Python-0-Starting/ex08/Loading.py
@@ -6,14 +6,17 @@ def ft_tqdm(lst: range) -> None:
     total = len(lst)
     if total == 0:
         return
+
     try:
-        len_term = max(60, os.get_terminal_size().columns - (34 + 2 * len(str(total))))
+        term_cols = os.get_terminal_size().columns
     except OSError:
-        len_term = 60
+        term_cols = 40
+    term_width = max(40, term_cols - (34 + 2 * len(str(total))))
+
     for i, element in enumerate(lst, 1):
         progress = i / total
-        bar = "█" * int(progress * len_term) + " " * (len_term -
-                                                      int(progress * len_term))
+        filled = progress * term_width
+        bar = "█" * int(filled) + " " * (term_width - int(filled))
         print(f"\r{progress * 100:3.0f}%|{bar}| {i}/{total}", end="")
         yield element
     print()
